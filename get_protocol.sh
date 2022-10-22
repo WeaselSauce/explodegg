426a5bcfdea9bbefe35516ade02d84f706b8ae8e
WeaselSauce <46676743+WeaselSauce@users.noreply.github.com>
2022-10-22 09:15:26 -0500

    Update get_protocol.sh

--- get_protocol.sh
+++ get_protocol.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+### requires monodis package to be installed on host (or inside container)
 SNIP=`monodis /home/rustserver/serverfiles/RustDedicated_Data/Managed/Rust.Global.dll | grep "int32 save"`
 RAW=`echo $SNIP | sed 's/.*(\(.*\))/\1/'`
 HEX=${RAW#"0x"}
