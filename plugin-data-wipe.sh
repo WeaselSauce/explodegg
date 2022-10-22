1a4439157526152d4bbf45339c7b34d116896ec2
WeaselSauce <46676743+WeaselSauce@users.noreply.github.com>
2022-10-22 07:46:09 -0500

    Create plugin-data-wipe.sh

---
+++ plugin-data-wipe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# clear mod files and folders
+rm /home/rustserver/serverfiles/oxide/data/NTeleportationHome.json
+rm /home/rustserver/serverfiles/oxide/data/NTeleportationTPR.json
+rm /home/rustserver/serverfiles/oxide/data/HammerRemove.json
+rm /home/rustserver/serverfiles/oxide/data/DynamicCupShare.json
+rm /home/rustserver/serverfiles/oxide/data/AutoCodeLock.json
+rm /home/rustserver/serverfiles/oxide/data/killstreak_player_data.json
+#rm /home/rustserver/serverfiles/oxide/data/SpawnSetWhitelists.json
+rm -rf /home/rustserver/serverfiles/oxide/data/KDRGui
+rm -rf /home/rustserver/serverfiles/oxide/data/Arkan
+#rm -rf /home/rustserver/serverfiles/oxide/data/Backpacks
